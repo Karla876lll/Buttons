5f9b35759b1148887221e6a2f5ecdfe601511e51
Karla876lll <201700252j.utea.edu.pe>
2021-06-06 02:23:47 -0500

    FINAL

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,71 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var city = ['Toronto', 'Boston', 'Mexico', 'London'];
+
+  var firstcity = 'toronto';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Leccion 06 Buttons'),
+          title: Text('Lesson 06: Buttons'),
         ),
         body: SafeArea(
           child: Column(children: <Widget>[
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                OutlineButton(
+                  shape: StadiumBorder(),
+                  highlightedBorderColor: Colors.blue,
+                  child: Text('Flights'),
+                  onPressed: () {},
+                ),
+                OutlineButton(
+                  shape: StadiumBorder(),
+                  highlightedBorderColor: Colors.blue,
+                  child: Text('Hotels'),
+                  onPressed: () {},
+                ),
+                OutlineButton(
+                  shape: StadiumBorder(),
+                  highlightedBorderColor: Colors.blue,
+                  child: Text('Cars'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            OutlineButton(
+              shape: StadiumBorder(),
+              highlightedBorderColor: Colors.blue,
+              child: Text('Outline Button'),
+              onPressed: () {},
+            ),
             Center(
               child: SafeArea(
-                child: Text(
-                  "Welcome to Android ATC",
-                  style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                      fontSize: 25.0),
-                ),
-              ),
+                  child: Text(
+                'Bienvenidos a Android ATC',
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                    fontSize: 25.0),
+              )),
             ),
-
-            // Botón levantado
+            //Raised Button
             RaisedButton(
               color: Colors.blue,
               child: Text(
-                'Boton Levantado',
+                'Raised Button',
                 style: TextStyle(
                     fontWeight: FontWeight.normal,
                     color: Colors.white,
@@ -36,16 +73,14 @@ class MyApp extends StatelessWidget {
               ),
               onPressed: () {},
             ),
-
-            // botón de icono
+            //Icon Buton
             IconButton(
               icon: Icon(Icons.volume_up),
-              tooltip: 'Aumneta el volumen en 10',
+              tooltip: 'Increase volume by 10',
               onPressed: () {},
             ),
-            Text('Sonido'),
-
-            // botón plano
+            Text('Sound'),
+            //Flat Button
             FlatButton(
               color: Colors.blue,
               textColor: Colors.white,
@@ -58,6 +93,23 @@ class MyApp extends StatelessWidget {
                 "Flat Button",
                 style: TextStyle(fontSize: 20.0),
               ),
+            ),
+
+            //start the code of dropdownButton//
+
+            DropdownButton<String>(
+              items: city.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              }).toList(),
+              onChanged: (String NewUserValue) {
+                setState(() {
+                  this.firstcity = NewUserValue;
+                });
+                // here your code will execute when the user select a new item
+              },
             ),
           ]),
         ),
